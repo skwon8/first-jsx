45bc0651fb3bfad44072506bd96d9d09276f76a7
skwon8 <saengsukwon@gmail.com>
2022-03-09 17:45:35 -0500

    first-jsx core assignment

--- src/App.js
+++ src/App.js
@@ -4,20 +4,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <ul>
+        <li>
           Learn React
-        </a>
-      </header>
+        </li>
+        <br></br>
+        <li>
+          Climb Mt. Everest
+        </li>
+        <br></br>
+        <li>
+          Run a marathon
+        </li>
+        <br></br>
+        <li>
+          Feed the dogs
+        </li>
+      </ul>
     </div>
   );
 }
